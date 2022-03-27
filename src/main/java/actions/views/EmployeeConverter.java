474df24ed383e75c632cf4a430f10a70d90f7f17
kazuhiro.kaita <sakuragi1940596@yahoo.co.jp>
2022-03-27 16:38:01 +0900

    Employeeモデル、EmployeeViewモデル作成（修正）

--- src/main/java/actions/views/EmployeeConverter.java
+++ src/main/java/actions/views/EmployeeConverter.java
@@ -1,4 +1,4 @@
-package views;
+package actions.views;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,19 +7,20 @@ import constants.AttributeConst;
 import constants.JpaConst;
 import models.Employee;
 
-//EmployeeDTOとEmployeeViewsの相互変換を行うためのクラス
-
+/**
+ * 従業員データのDTOモデル⇔Viewモデルの変換を行うクラス
+ *
+ */
 public class EmployeeConverter {
-    /* ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
-     * @param ev EmployeeViewのインスタンス
-     * @return Employeeのインスタンス*/
 
-    //EmployeeViewのデータ型を持つev変数に引数を渡して戻り値を返すメソッド
+    /**
+     * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
+     * @param ev EmployeeViewのインスタンス
+     * @return Employeeのインスタンス
+     */
     public static Employee toModel(EmployeeView ev) {
 
-        //戻り値としてEmployeeクラスのインスタンスオブジェクトを返す
         return new Employee(
-                //getterを使って、EmployeeViewオブジェクトの各フィールドの値をEmployeeオブジェクトに代入
                 ev.getId(),
                 ev.getCode(),
                 ev.getName(),
@@ -43,15 +44,13 @@ public class EmployeeConverter {
      * @param e Employeeのインスタンス
      * @return EmployeeViewのインスタンス
      */
-
     public static EmployeeView toView(Employee e) {
 
         if(e == null) {
             return null;
         }
-        //EmployeeViesのインスタンスオブジェクトを戻り値して返す
+
         return new EmployeeView(
-                //EmployeeViewオブジェクトの各フィールドに、Employeeオブジェクトの各値を代入
                 e.getId(),
                 e.getCode(),
                 e.getName(),
@@ -69,30 +68,28 @@ public class EmployeeConverter {
                                 ? AttributeConst.DEL_FLAG_TRUE.getIntegerValue()
                                 : AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
     }
+
     /**
      * DTOモデルのリストからViewモデルのリストを作成する
      * @param list DTOモデルのリスト
      * @return Viewモデルのリスト
      */
-    //ジェネリクスをEmployeeView型に指定して、配列listを生成
     public static List<EmployeeView> toViewList(List<Employee> list) {
-        //配列evsを生成
         List<EmployeeView> evs = new ArrayList<>();
-        //拡張for文を利用して、list配列内の値を順次取り出し、その値をEmployee型変数eとして、配列evsに加える
+
         for (Employee e : list) {
             evs.add(toView(e));
         }
 
         return evs;
     }
+
     /**
      * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
      * @param e DTOモデル(コピー先)
      * @param ev Viewモデル(コピー元)
      */
     public static void copyViewToModel(Employee e, EmployeeView ev) {
-
-        //Employeeオブジェクトに、EmployeeViewオブジェクトの各フィールドの値をコピーする
         e.setId(ev.getId());
         e.setCode(ev.getCode());
         e.setName(ev.getName());
@@ -101,5 +98,7 @@ public class EmployeeConverter {
         e.setCreatedAt(ev.getCreatedAt());
         e.setUpdatedAt(ev.getUpdatedAt());
         e.setDeleteFlag(ev.getDeleteFlag());
+
     }
+
 }
